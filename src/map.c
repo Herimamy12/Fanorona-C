1c964065661f37593b3647b14eb82f016609336b
Nirina Prosper Herimaminavalona <nherimam@c1r13s2.42antananarivo.mg>
2024-06-24 10:54:30 +0300

    0

---
+++ src/map.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nherimam <nherimam@student.42antanana      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/24 10:34:35 by nherimam          #+#    #+#             */
+/*   Updated: 2024/06/24 10:50:33 by nherimam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "structure.h"
+
+char	**get_char_map(int fd);
+
+t_map	*new_map(int w, int h)
+{
+	int 	fd;
+	t_map	*map;
+
+	fd = open ("map3", O_RDONLY);
+	if (fd < 0)
+		return (NULL);
+	map->map = get_char_map (fd);
+	map->w = w;
+	map->h = h;
+	return (map);
+}
+
+
+char	**get_char_map(int fd)
+{
+	int		i;
+	char	*tmp;
+	char	*line;
+	char	**charmap;
+
+	tmp = NULL;
+	line = get_next_line (fd);
+	i = 0;
+	while (line)
+	{
+		tmp = ft_strjoin (tmp, line);
+		free (line);
+		line = get_next_line (fd);
+		i++;
+	}
+	charmap = ft_split (tmp, '\n');
+	if (!charmap)
+		return (NULL);
+	free (tmp);
+	free (line);
+	return (charmap);
+}
